5242a33d67180f4721acca88d7dc6ad63824af33
Nicola <Nicola>
2025-05-04 01:14:30 +0200

    Add setup script for loading packages

---
+++ R_project_colab_git.R
@@ -0,0 +1,97 @@
+rm(list=ls())
+
+#Libraries installation
+library(dplyr)
+library(lubridate)
+library("ggplot2")
+library(tidyverse)
+library(car) # vif
+library(glmnet) # LASSO
+library(moments) # jarque.test
+library(caret) # validazione esterna
+library(DataExplorer)
+
+print("Libraries loaded")
+
+
+#Dataset import
+
+library(readr)
+Agrimonia_Dataset <- read_csv("/Users/nicolasilvestri/Desktop/Unibg/Statistics/PART 1/R scripts and data/Databases/Agrimonia_Dataset_v_3_0_0.csv") #From .csv
+Metadata_stations <- read_csv("/Users/nicolasilvestri/Desktop/Unibg/Statistics/PART 1/R scripts and data/Databases/Metadata_monitoring_network_registry_v_2_0_1.csv")
+
+# load(file = "Agrimonia_Dataset_v_3_0_0.Rdata") #From R
+# Agrimonia_Dataset <- AgrImOnIA_Dataset_v_3_0_0
+# rm(list="AgrImOnIA_Dataset_v_3_0_0")
+
+Stations_name <- Metadata_stations %>%
+  select(IDStation, NameStation, Province) %>%
+  distinct(IDStation, .keep_all = TRUE)
+
+Agrimonia_Dataset <- Agrimonia_Dataset %>%
+  left_join(Stations_name, 
+            by = c("IDStations" = "IDStation")) %>%
+  select(IDStations, NameStation, Province, everything())
+
+Agrimonia_Dataset$Month <- month(Agrimonia_Dataset$Time, label = TRUE)
+Agrimonia_Dataset <- Agrimonia_Dataset %>% select(IDStations:Time, Month, everything())
+mesi_italiani <- c("Jan", "Feb", "Mar", "Apr", "May", "Jun",
+                   "Jul", "Aug", "Sep", "Oct", "Nov", "Dec")
+Agrimonia_Dataset <- Agrimonia_Dataset %>%
+  mutate(
+    Month_num = match(Month, mesi_italiani),  
+    Season = case_when(
+      Month_num %in% c(12, 1, 2)  ~ "Winter",
+      Month_num %in% c(3, 4, 5)   ~ "Spring",
+      Month_num %in% c(6, 7, 8)   ~ "Summer",
+      Month_num %in% c(9, 10, 11)   ~ "Autumn",
+    ))
+
+
+# Create a vector with station IDs selected
+stations_id   <- c(504, 583, 697)
+# stations_name <- c("Sesto San Giovanni -> MI", "Bergamo -> BG", "Borgofranco sul Po -> MN")
+
+DB <- Agrimonia_Dataset %>% 
+  filter(IDStations %in% stations_id)
+MI_DB <- DB %>% 
+  filter(IDStations==504)
+BG_DB <- DB %>% 
+  filter(IDStations==583)
+MN_DB <- DB %>% 
+  filter(IDStations==697)
+
+
+#Preliminary data analysis
+
+#Global analysis
+plot(DB$Time,DB$AQ_nox)
+plot(DB$Month,DB$AQ_nox)
+ggplot(DB, aes(x = Season, y = AQ_nox)) +
+  geom_boxplot()+ theme_minimal() +
+  theme(legend.position = "bottom")
+ggplot(DB, aes(x = Time, y = AQ_nox, color=Province)) +
+  geom_point()+ theme_minimal() +
+  theme(legend.position = "bottom")
+ggplot(DB, aes(x = Season, y = AQ_nox, color=Province)) +
+  geom_boxplot()+ theme_minimal() +
+  theme(legend.position = "bottom")
+ggplot(DB, aes(x = Time, y = AQ_nox, color=Province)) +
+  geom_smooth()+ theme_minimal() +
+  theme(legend.position = "bottom")
+ggplot(DB, aes(x  = AQ_nox, color=Province)) +
+  geom_density(size=1.5)+ theme_minimal() +
+  theme(legend.position = "bottom")
+# Create full HTML report
+create_report(DB,
+              y = "Province",
+              config = configure_report(add_plot_prcomp = FALSE),
+              output_file = "EDA_Prov_Report.html")
+
+ggplot(DB, aes(x = Time, y = AQ_nox)) +
+  geom_point() + 
+  geom_smooth(method = "lm", se = FALSE) +
+  facet_wrap(~ Province) +
+  labs(title = "AQ_nox over time by Province") +
+  theme_minimal() +
+  theme(legend.position = "bottom")
